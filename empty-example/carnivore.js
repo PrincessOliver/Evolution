503fcfe6a3ad393ca9c67f53b82329857e8b9a4e
PrincessOliver <90751021+PrincessOliver@users.noreply.github.com>
2021-12-03 18:38:27 +0100

    Add files via upload

---
+++ empty-example/carnivore.js
@@ -0,0 +1,172 @@
+function Carnivore(x, y, dna) {
+  this.acceleration = createVector(0, 0);
+  this.velocity = createVector(0, -2);
+  this.position = createVector(x, y);
+  this.r = 4;
+  this.maxspeed = 16;
+  this.maxforce = 0.2;
+  this.target = createVector(x, y);
+
+  this.health = 1;
+  this.dna = [];
+  if (dna === undefined) {
+    // Food weight
+    this.dna[0] = random(-2, 2);
+    // Poison weight
+    this.dna[1] = random(-2, 2);
+    // Food perception
+    this.dna[2] = random(0, 100);
+    // Poision Percepton
+    this.dna[3] = random(0, 100);
+  } else {
+    // Mutation
+    this.dna[0] = dna[0];
+    if (random(1) < mr) {
+      this.dna[0] += random(-0.1, 0.1);
+    }
+    this.dna[1] = dna[1];
+    if (random(1) < mr) {
+      this.dna[1] += random(-0.1, 0.1);
+    }
+    this.dna[2] = dna[2];
+    if (random(1) < mr) {
+      this.dna[2] += random(-10, 10);
+    }
+    this.dna[3] = dna[3];
+    if (random(1) < mr) {
+      this.dna[3] += random(-10, 10);
+    }
+  }
+
+  //Method to update Carnivore location
+  this.update = function () {
+    // Update velocity
+    this.velocity.add(this.acceleration);
+    // Limit speed
+    this.velocity.limit(this.maxspeed);
+    this.position.add(this.velocity);
+    // Reset accelerationelertion to 0 each cycle
+    this.acceleration.mult(0);
+  };
+  this.applyForce = function (force) {
+    //We could add mass here if we want
+    this.acceleration.add(force);
+  };
+
+  // A method that calculates a steering force
+  // STEER = DESIRED MINUS VELOCITY
+  this.seek = function (target) {
+    var desired = p5.Vector.sub(target, this.position); //
+
+    desired.setMag(this.maxspeed);
+
+    // Steering = Desired minus velocity
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxforce); // Limit to maximum steering force
+
+    this.applyForce(steer);
+  };
+  this.behaviors = function (good) {
+    // This controls towards poison/food
+    var steerG = this.eat(good, 0.2, this.dna[2]);
+    //var steerB = this.eat(bad, -1, this.dna[3]);
+
+    // steerG.mult(this.dna[0]);
+    //steerB.mult(this.dna[1]);
+
+    this.applyForce(steerG);
+    //  this.applyForce(steerB);
+  };
+
+  this.eat = function (list, nutrition, perception) {
+    //Vehicle Collison with food
+    var record = Infinity;
+    var closest = null;
+    for (var i = list.length - 1; i >= 0; i--) {
+      var d = this.position.dist(list[i]);
+
+      if (d < this.maxspeed) {
+        list.splice(i, 1);
+        this.health += nutrition;
+      } else {
+        if (d < record && d < perception) {
+          record = d;
+          closest = list[i];
+        }
+      }
+    }
+
+    // This is the moment of eating!
+
+    if (closest != null) {
+      return this.seek(closest);
+    }
+
+    return createVector(0, 0);
+  };
+
+  this.display = function () {
+    // Draw a triangle rotated in the direction of velocity
+    var angle = this.velocity.heading() + PI / 2;
+
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(angle);
+
+    if (debug.checked()) {
+      strokeWeight(3);
+      stroke(0, 255, 0);
+      noFill();
+      line(0, 0, 0, -this.dna[0] * 25);
+      strokeWeight(2);
+      ellipse(0, 0, this.dna[2] * 2);
+      stroke(255, 0, 0);
+      line(0, 0, 0, -this.dna[1] * 25);
+      ellipse(0, 0, this.dna[3] * 2);
+    }
+
+    var gr = color(255, 255, 0);
+    var rd = color(255, 0, 0);
+    var col = lerpColor(rd, gr, this.health);
+
+    imageMode(CENTER);
+    image(img2,0,0);
+    img2.resize(20, 20);
+/*
+    fill(col);
+    stroke(col);
+    strokeWeight(1);
+    beginShape();
+    vertex(0, -this.r * 2);
+    vertex(-this.r, this.r * 2);
+    vertex(this.r, this.r * 2);
+    endShape(CLOSE);*/
+
+    pop();
+  };
+  this.boundaries = function () {
+    var d = 25;
+
+    var desired = null;
+
+    if (this.position.x < d) {
+      desired = createVector(this.maxspeed, this.velocity.y);
+    } else if (this.position.x > width - d) {
+      desired = createVector(-this.maxspeed, this.velocity.y);
+    }
+
+    if (this.position.y < d) {
+      desired = createVector(this.velocity.x, this.maxspeed);
+    } else if (this.position.y > height - d) {
+      desired = createVector(this.velocity.x, -this.maxspeed);
+    }
+
+    if (desired !== null) {
+      desired.normalize();
+      desired.mult(this.maxspeed);
+      var steer = p5.Vector.sub(desired, this.velocity);
+      steer.limit(this.maxforce);
+      this.applyForce(steer);
+    }
+  };
+}
---
+++ empty-example/sketch.js
@@ -0,0 +1,141 @@
+
+var img;
+var img2;
+var img3;
+var img4;
+var img5;
+var img6;
+var carnivores = [];
+var vehicles = [];
+var food = [];
+var poison = [];
+
+var debug;
+
+function setup() {
+  createCanvas(1200, 550);
+  img=loadImage("Png til skole/Stem3.png.png");
+  img2=loadImage("Png til skole/Carnivore2.png.png");
+  img3=loadImage("Png til skole/perceptionbug.png.png");
+  img4=loadImage("Png til skole/poisonperceptionbug.png.png");
+  img5=loadImage("Png til skole/ultimateperceptionbug.png.png");
+  img6=loadImage("Png til skole/Bigboi.png.png");
+
+
+  for (var i = 0; i < 50; i++) {
+    //Spawn Vehicles and positioning of them.
+    var x = random(width);
+    var y = random(height);
+    vehicles[i] = new Vehicle(x, y);
+  }
+
+/*
+  for (var i = 0; i < 10; i++) {
+    //Spawn Vehicles and positioning of them.
+    var x = random(width);
+    var y = random(height);
+    carnivores[i] = new Carnivore(x, y);
+  }
+  */
+
+  for (var i = 0; i < 60; i++) {
+    //Spawn Food
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  for (var i = 0; i < 20; i++) {
+    //Spawn poison
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
+  }
+
+  debug = createCheckbox(); //
+}
+
+function mouseDragged() { //Testing vehicles by clicking mouse
+  vehicles.push(new Vehicle(mouseX, mouseY));
+}
+
+/*function mouseDragged() {
+  //Testing vehicles by clicking mouse
+  carnivores.push(new Carnivore(mouseX, mouseY));
+}*/
+
+function draw() {
+  background(51);
+  //image(img,0,0);
+  fill(3, 78, 252);
+  rect(250, 0, 50, 1000);
+
+  for (i = 0; i < vehicles.length; i++){
+      textSize(32);
+      text("Creatures Alive: "+vehicles.length, 10, 30);
+      fill(252, 3, 3);
+    }
+
+  for (i = 0; i < carnivores.length; i++){
+      textSize(32);
+      text("Carnivores Alive: "+carnivores.length, 10, 60);
+      fill(252, 3, 3);
+    }
+
+  if (random(1) < 0.5) {
+    //Spawning food randomly at different intevals
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
+
+
+  if (random(1) < 0.01) {
+    //Spawning poison randomly at different intevals
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
+  }
+
+  for (var i = 0; i < food.length; i++) {
+    //Food Visuals
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(food[i].x, food[i].y, 4, 4);
+  }
+
+  for (var i = 0; i < poison.length; i++) {
+    //Poison Visuals
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 4, 4);
+  }
+
+  for (var i = vehicles.length - 1; i >= 0; i--) {
+    vehicles[i].boundaries();
+    vehicles[i].behaviors(food, poison);
+    vehicles[i].update(); //Updates position of Vehicle
+    vehicles[i].display(); // Visuals for Vehicle
+
+    var newVehicle = vehicles[i].clone();
+    if (newVehicle != null) {
+      vehicles.push(newVehicle);
+    }
+
+    if (vehicles[i].dead()) {
+      var x = vehicles[i].position.x;
+      var y = vehicles[i].position.y;
+      food.push(createVector(x, y));
+      vehicles.splice(i, 1);
+    }
+  }
+
+  for (var i = carnivores.length - 1; i >= 0; i--) {
+    carnivores[i].boundaries();
+    carnivores[i].behaviors(food, poison);
+    carnivores[i].update(); //Updates position of Vehicle
+    carnivores[i].display(); // Visuals for Vehicle
+
+
+  }
+}
